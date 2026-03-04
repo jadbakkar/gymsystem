e2743b7e744d9c3f2b6a678631c016a378ed1b39
jadbakkar <jadbakkar03@gmail.com>
2026-03-04 13:22:58 +0200

    Update db.php

--- gym_demo/admin/db.php
+++ gym_demo/admin/db.php
@@ -2,10 +2,11 @@
     
 
     $db_server = "localhost";
-    $db_user = "root";
-    $db_password = "";
-    $db_name = "gum";
+    $db_user = "db_username";
+    $db_password = "db_password";
+    $db_name = "gym_system";
     $conn = "";
 
     $conn = mysqli_connect($db_server, $db_user, $db_password, $db_name);
+
 ?>
